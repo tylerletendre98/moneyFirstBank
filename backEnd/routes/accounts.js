1e85e3760db7ca597b276bd721f236e98588c4cc
T U <tylerletendre98@gmail.com>
2022-03-29 09:50:31 -0400

    was able to figure out why the if statement wasnt working had to convert object id from DB first but can now change the amount of money in the account

--- backEnd/routes/accounts.js
+++ backEnd/routes/accounts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user')
 const Account = require('../models/account');
+const { type } = require('express/lib/response');
 const router = express.Router();
 
 router.post('/newAccount/:userId', async(req,res)=>{
@@ -18,13 +19,18 @@ router.post('/newAccount/:userId', async(req,res)=>{
     }
 })
 
-router.post('/depositMoney/:accountId', async(req,res)=>{
+router.post('/depositMoney/:accountId/:userId', async(req,res)=>{
     try{
+        const user = await User.findById(req.params.userId)
         const account = await Account.findById(req.params.accountId)
-        const depositAmount = req.body.depositAmount
-        account.balance += depositAmount
-        account.save()
-        return res.send(account)
+        const depositMoney = req.body.depositMoney
+        for (let i = 0; i < user.accounts.length; i++) {
+            if(String(user.accounts[i]._id) === String(account._id)){
+                user.accounts[i].balance += depositMoney
+                user.save()
+                return res.send(user)
+            }
+        }
     }catch(ex){
         return res.status(500).send(`Internal Server Error ${ex}.`)
     }
