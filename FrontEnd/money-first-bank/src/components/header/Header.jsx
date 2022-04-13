a4b268a3d1ad738d16a91fa98cc3a4bf464e0dea
T U <tylerletendre98@gmail.com>
2022-04-12 21:08:32 -0400

    adding in initial structure to the front end

---
+++ FrontEnd/money-first-bank/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div>
+      <div className="header-title">
+        <h1>Money First Bank</h1>
+      </div>
+      <div>
+        <div>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ FrontEnd/money-first-bank/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>this is the login page</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
