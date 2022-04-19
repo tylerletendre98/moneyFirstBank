e691992454b08e453b97df64807fe39da25d82b3
T U <tylerletendre98@gmail.com>
2022-04-19 12:30:43 -0400

    condtionally rendering the header that way when you click the login button it changed the login button to a home button that brings you back to the main page. Also added in the inputs for the login page

--- FrontEnd/money-first-bank/src/components/header/Header.jsx
+++ FrontEnd/money-first-bank/src/components/header/Header.jsx
@@ -1,19 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return (
-    <div className="header-container">
-      <div className="header-title">
-        <h1>Money First Bank</h1>
-      </div>
-      <div className="header-buttons-container">
-        <div className="header-button">
-          <button>Login</button>
+  const [loggingIn, setLoggingIn] = useState(false);
+
+  if (loggingIn === false) {
+    return (
+      <div className="header-container">
+        <div className="header-title">
+          <h1>Money First Bank</h1>
+        </div>
+        <div className="header-buttons-container">
+          <div className="header-button">
+            <Link to={"/loginPage"}>
+              <button onClick={() => setLoggingIn(!loggingIn)}>Login</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="header-container">
+        <div className="header-title">
+          <h1>Money First Bank</h1>
+        </div>
+        <div className="header-buttons-container">
+          <div className="header-button">
+            <Link to={"/"}>
+              <button onClick={() => setLoggingIn(!loggingIn)}>Home</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- FrontEnd/money-first-bank/src/pages/LoginPage.jsx
+++ FrontEnd/money-first-bank/src/pages/LoginPage.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
-      <h1>this is the login page</h1>
+      <div>
+        <label htmlFor="">Username:</label>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="">password:</label>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
