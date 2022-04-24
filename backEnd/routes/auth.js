73761b1ef1915b0781b4765bbc0aef842c889ad2
T U <tylerletendre98@gmail.com>
2022-04-24 14:28:08 -0400

    finished the auth file which sends the token to the front end

--- backEnd/routes/auth.js
+++ backEnd/routes/auth.js
@@ -1,11 +1,21 @@
 const bcrypt = require('bcrypt');
 const express = require('express');
 const {User} = require('../models/user');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post('/login', async(req,res)=>{
     try {
+        const user = await User.findOne({email:req.body.email});
+        if(!user) return res.status(400).send('Invaild Email or password');
+
+        const validPassword = await bcrypt.compare(req.body.validPassword, user.password);
+        if(!validPassword) return res.status(400).send('Invalid email or password');
         
+        const token = jwt.sign({_id: user._id,name: user.name}, config.get('jwtSecret'));
+
+        return res.send(token)
     } catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
