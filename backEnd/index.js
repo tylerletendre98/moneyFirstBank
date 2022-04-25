8d9103e0e5d8b3aa5e6307559fc1fbeabe9453ad
T U <tylerletendre98@gmail.com>
2022-04-25 19:52:27 -0400

    trying to get the jwt token to create after finding the right user in the array of users

--- backEnd/index.js
+++ backEnd/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const users = require('./routes/users')
 const accounts = require('./routes/accounts')
+const auth = require('./routes/auth');
 const app = express();
 
 
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/accounts', accounts)
+app.use('/api/auth', auth )
 
 
 const port = process.env.PORT || 5000;
--- backEnd/routes/auth.js
+++ backEnd/routes/auth.js
@@ -1,22 +1,24 @@
 const bcrypt = require('bcrypt');
 const express = require('express');
-const {User} = require('../models/user');
+const User = require('../models/user');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post('/login', async(req,res)=>{
     try {
-        const user = await User.findOne({email:req.body.email});
+        const user = await User.findOne({ email:req.body.email });
         if(!user) return res.status(400).send('Invaild Email or password');
 
         const validPassword = await bcrypt.compare(req.body.validPassword, user.password);
         if(!validPassword) return res.status(400).send('Invalid email or password');
         
-        const token = jwt.sign({_id: user._id,name: user.name}, config.get('jwtSecret'));
+        const token = jwt.sign({fullName:user.fullName, email: user.email, accounts: user.accounts}, config.get('jwtSecret'));
 
         return res.send(token)
     } catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 })
+
+module.exports = router
