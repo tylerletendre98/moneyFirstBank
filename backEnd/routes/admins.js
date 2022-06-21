5aa047ec0018069e565de161d61b8f16038d6f33
T U <tylerletendre98@gmail.com>
2022-06-21 17:50:52 -0400

    admin can now approve accounts

--- backEnd/routes/admins.js
+++ backEnd/routes/admins.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Admin = require('../models/admin')
+const config = require('config');
+const User = require('../models/user');
 
 router.post('/newAdmin', async(req,res)=>{
     try{
@@ -17,9 +19,24 @@ router.post('/newAdmin', async(req,res)=>{
 
 router.get('/getAdmin', async(req,res)=>{
     try{
-        const admin = await Admin.find()
+        const admin = await Admin.findById(config.get('AdminId'))
         return res.send(admin)
     }catch(ex){
+        return res.status(500).send(`Internal Server Error ${ex}.`)
+    }
+})
+
+router.put('/approveUser/:userId', async(req,res)=>{
+    try {
+        const admin = await Admin.findById(config.get('AdminId'))
+        const user = await User.findById(req.params.userId)
+        user.isApproved = true
+        user.save()
+        admin.usersToBeApproved = admin.usersToBeApproved.filter((users)=> users === user)
+        admin.save()
+        return res.send(admin)
+
+    } catch (ex) {
         return res.status(500).send(`Internal Server Error ${ex}.`)
     }
 })
